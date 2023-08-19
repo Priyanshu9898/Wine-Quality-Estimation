56d3fc0b199328a4b6876ed181cd95f42ff3c91f
Priyanshu9898 <priyanshumalaviya9210@gmail.com>
2023-08-19 15:38:57 +0530

    working on form, some input box is created

--- frontend/components/Form.tsx
+++ frontend/components/Form.tsx
@@ -1,52 +1,73 @@
-import React, { FC } from "react";
+"use client";
+
+import React, { FC, useState } from "react";
 
 const Form: FC = () => {
+  const [data, setData] = useState({
+    fixed_acidity: "",
+    volatile_acidity: "",
+    citric_acid: "",
+  });
+
+  const handleChange = (e: any) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
       <form>
         <div className="grid gap-6 mb-6 md:grid-cols-2">
           <div>
             <label
-              htmlFor="first_name"
+              htmlFor="fixed_acidity"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              First name
+              Fixed Acidity
             </label>
             <input
               type="text"
-              id="first_name"
+              id="fixed_acidity"
+              name="fixed_acidity"
+              value={data.fixed_acidity}
+              onChange={handleChange}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="John"
+              placeholder="fixed_acidity"
               required
             />
           </div>
           <div>
             <label
-              htmlFor="last_name"
+              htmlFor="volatile_acidity"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Last name
+              Volatile Acidity
             </label>
             <input
               type="text"
-              id="last_name"
+              id="volatile_acidity"
+              name="volatile_acidity"
+              value={data.volatile_acidity}
+              onChange={handleChange}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Doe"
+              placeholder="volatile_acidity"
               required
             />
           </div>
           <div>
             <label
-              htmlFor="company"
+              htmlFor="citric_acid"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Company
+              Citric Acid
             </label>
             <input
               type="text"
-              id="company"
+              id="citric_acid"
+              name="citric_acid"
+              value={data.citric_acid}
+              onChange={handleChange}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Flowbite"
+              placeholder="citric_acid"
               required
             />
           </div>
@@ -58,7 +79,7 @@ const Form: FC = () => {
               Phone number
             </label>
             <input
-              type="tel"
+              type="text"
               id="phone"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="123-45-678"
--- frontend/components/InputCard.tsx
+++ frontend/components/InputCard.tsx
@@ -4,7 +4,7 @@ import Form from "./Form";
 const InputCard: FC = () => {
   return (
     <>
-      <div className="max-w-6xl mt-6 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-7xl mt-6 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <Form />
       </div>
     </>
